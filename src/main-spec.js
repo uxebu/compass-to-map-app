e474b876a2151f94176eca8011d90400eac7737d
Wolfram Kriesing <wk@uxebu.com>
2014-11-12 13:09:32 +0100

    Use util and rename update method.

--- src/main-spec.js
+++ src/main-spec.js
@@ -1,21 +1,21 @@
-var main = require('./main');
-var image = main.image;
-var scrollDetection = main.scrollDetection;
+var util = require('./util');
+var image = util.image;
+var scrollDetection = util.scrollDetection;
 
 describe('when scrolling', function() {
   it('should update the image rotation', function() {
     spyOn(image, 'rotate');
-    scrollDetection.onUpdate();
+    scrollDetection.update();
     expect(image.rotate).toHaveBeenCalled();
   });
   it('should rotate 1º for 1 pixel scrolled down', function() {
     spyOn(image, 'rotate');
-    scrollDetection.onUpdate(1);
+    scrollDetection.update(1);
     expect(image.rotate).toHaveBeenCalledWith(1);
   });
   it('should rotate 1º for 361 pixel scrolled down', function() {
     spyOn(image, 'rotate');
-    scrollDetection.onUpdate(361);
+    scrollDetection.update(361);
     expect(image.rotate).toHaveBeenCalledWith(1);
   });
 });
