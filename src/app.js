7a5f1ffd46fe1b339cb8ec3022253ac2f1f97490
Wolfram Kriesing <wk@uxebu.com>
2014-11-17 10:44:54 +0100

    Refactor App a bit.

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ function App(domUtil, convert) {
   this._domUtil = domUtil;
   this._convert = convert;
 }
+
 App.prototype = {
   start: function() {
-    var convert = this._convert;
-    var self = this;
-    this._domUtil.onScroll(function(scrollPos) {
-      self._rotateByDegrees(convert.scrollPositionToDegrees(scrollPos));
-    });
-    this._domUtil.onDeviceOrientationChange(function(event) {
-      self._rotateByDegrees(convert.deviceOrientationEventToDegrees(event));
-    });
+    this._domUtil.onScroll(this._rotateByScrollPosition.bind(this));
+    this._domUtil.onDeviceOrientationChange(this._rotateByDeviceOrienationEvent.bind(this));
+  },
+
+  _rotateByScrollPosition: function(scrollPos) {
+    this._rotateByDegrees(this._convert.scrollPositionToDegrees(scrollPos));
+  },
+
+  _rotateByDeviceOrienationEvent: function(event) {
+    this._rotateByDegrees(this._convert.deviceOrientationEventToDegrees(event));
   },
 
   _rotateByDegrees: function(degrees) {
