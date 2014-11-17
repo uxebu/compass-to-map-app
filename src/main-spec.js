615c24307028a6cb2fb9b1af1d10695b28024c77
Wolfram Kriesing <wk@uxebu.com>
2014-11-17 09:38:22 +0100

    Hook up device orientation change in app.start.

--- src/main-spec.js
+++ src/main-spec.js
@@ -1,11 +1,15 @@
 var domUtil = {
   rotate: function() {},
-  onScroll: function() {}
+  onScroll: function() {},
+  onDeviceOrientationChange: function() {}
 };
 
 var app = {
   start: function() {
     domUtil.onScroll(function(degrees) {
+      domUtil.rotate(degrees);
+    });
+    domUtil.onDeviceOrientationChange(function(degrees) {
       domUtil.rotate(degrees);
     });
   }
@@ -16,10 +20,6 @@ describe('on app start rotate on an event', function() {
   beforeEach(function() {
     spyOn(domUtil, 'rotate');
   });
-
-  function emulateDeviceOrientationChange(degrees) {
-    domUtil.rotate(degrees);
-  }
 
   describe('on scroll', function() {
     var onScrollCallback;
@@ -44,9 +44,21 @@ describe('on app start rotate on an event', function() {
       expect(domUtil.rotate).toHaveBeenCalledWith(degrees);
     });
   });
-  it('should be triggered on deviceorientation change', function() {
-    var degrees = 42;
-    emulateDeviceOrientationChange(degrees);
-    expect(domUtil.rotate).toHaveBeenCalledWith(degrees);
+  describe('on deviceorientation change', function() {
+    var onDeviceOrientationChangeCallback;
+    beforeEach(function() {
+      spyOn(domUtil, 'onDeviceOrientationChange').andCallFake(function(cb) { onDeviceOrientationChangeCallback = cb; });
+    });
+
+    function callOnDeviceOrientationChangeCallback(degrees) {
+      onDeviceOrientationChangeCallback(degrees);
+    }
+    
+    it('when event fires from the DOM it shall rotate', function() {
+      var degrees = 42;
+      app.start();
+      callOnDeviceOrientationChangeCallback(degrees);
+      expect(domUtil.rotate).toHaveBeenCalledWith(degrees);
+    });
   });
 });
