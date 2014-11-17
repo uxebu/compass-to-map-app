0ca57cccdf58389dc34cb444915dcf4bbc371ab3
Wolfram Kriesing <wk@uxebu.com>
2014-11-17 11:22:02 +0100

    Inline functions, to use the scope variable domUtilMock.

--- src/app-spec.js
+++ src/app-spec.js
@@ -13,7 +13,7 @@ describe('after app start', function() {
   it('rotate on scroll', function() {
     mockedDomUtil.hasDeviceOrientation.andReturn(false);
     var scrollOffset = 42;
-    fakeAScrollTo(scrollOffset, mockedDomUtil);
+    fakeAScrollTo(scrollOffset);
 
     expect(mockedDomUtil.rotate).toHaveBeenCalledWith(scrollOffset);
   });
@@ -21,7 +21,7 @@ describe('after app start', function() {
   it('rotate on deviceorientation change', function() {
     mockedDomUtil.hasDeviceOrientation.andReturn(true);
     var degrees = 23;
-    fakeADeviceOrientationChangeTo(degrees, mockedDomUtil);
+    fakeADeviceOrientationChangeTo(degrees);
 
     expect(mockedDomUtil.rotate).toHaveBeenCalledWith(degrees);
   });
@@ -29,26 +29,26 @@ describe('after app start', function() {
   it('should only use deviceorientation if available', function() {
     mockedDomUtil.hasDeviceOrientation.andReturn(true);
 
-    startApp(mockedDomUtil);
+    startApp();
 
     expect(mockedDomUtil.onScroll).not.toHaveBeenCalled();
   });
+
+  function startApp() {
+    var app = new App(mockedDomUtil, mockedConvert);
+    app.start();
+  }
+
+  function fakeAScrollTo(scrollOffset) {
+    mockedConvert.scrollPositionToDegrees.andReturn(scrollOffset);
+    mockedDomUtil.onScroll.andCallFake(function(cb) { cb(scrollOffset); });
+    startApp();
+  }
+
+  function fakeADeviceOrientationChangeTo(degrees) {
+    mockedConvert.deviceOrientationEventToDegrees.andReturn(degrees);
+    mockedDomUtil.onDeviceOrientationChange.andCallFake(function(cb) { cb(degrees); });
+    startApp();
+  }
+
 });
-
-function startApp(mockedDomUtil) {
-  var app = new App(mockedDomUtil, mockedConvert);
-  app.start();
-}
-
-function fakeAScrollTo(scrollOffset, mockedDomUtil) {
-  mockedConvert.scrollPositionToDegrees.andReturn(scrollOffset);
-  mockedDomUtil.onScroll.andCallFake(function(cb) { cb(scrollOffset); });
-  startApp(mockedDomUtil);
-}
-
-function fakeADeviceOrientationChangeTo(degrees, mockedDomUtil) {
-  mockedConvert.deviceOrientationEventToDegrees.andReturn(degrees);
-  mockedDomUtil.onDeviceOrientationChange.andCallFake(function(cb) { cb(degrees); });
-  startApp(mockedDomUtil);
-}
-
