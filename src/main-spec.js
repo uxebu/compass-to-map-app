6ae3a42a1a3a54f52c7a64a61632764b8a32f288
Wolfram Kriesing <wk@uxebu.com>
2014-11-17 09:23:43 +0100

    Deviceorientation change should also rotate.

--- src/main-spec.js
+++ src/main-spec.js
@@ -4,12 +4,21 @@ var domUtil = {
 function emulateDocumentScrollEvent(degrees) {
   domUtil.rotate(degrees);
 }
+function emulateDeviceOrientationChange(degrees) {
+  domUtil.rotate(degrees);
+}
 
 describe('rotate on an event', function() {
   it('should be triggered on scroll', function() {
     var degrees = 42;
     spyOn(domUtil, 'rotate');
     emulateDocumentScrollEvent(degrees);
+    expect(domUtil.rotate).toHaveBeenCalledWith(degrees);
+  });
+  it('should be triggered on deviceorientation change', function() {
+    var degrees = 42;
+    spyOn(domUtil, 'rotate');
+    emulateDeviceOrientationChange(degrees);
     expect(domUtil.rotate).toHaveBeenCalledWith(degrees);
   });
 });
