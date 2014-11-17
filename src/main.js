2178bcd1b907fa51c3fbe1a24fb58e1c27f81f61
Wolfram Kriesing <wk@uxebu.com>
2014-11-17 16:29:06 +0100

    Hook up all in main app, intergration tests missing.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 var App = require('./app');
 var domUtil = require('./domUtil');
 var convert = require('./convert');
+var DeviceRotationApp = require('./deviceorientation-behavior-app');
+var ScrollApp = require('./scroll-behavior-app');
 
-new App(domUtil, convert).start();
+var deviceRotationApp = new DeviceRotationApp(domUtil, convert);
+var scrollApp = new ScrollApp(domUtil, convert);
+
+new App(domUtil, scrollApp, deviceRotationApp).start();
