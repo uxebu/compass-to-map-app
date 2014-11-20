1a4d155cc6ca861441356676b11e4fc285ec14f0
Wolfram Kriesing <wk@uxebu.com>
2014-11-20 12:26:47 +0100

    Properly import class using a different name.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ var App = require('./app');
 var domUtil = require('./domUtil');
 var convert = require('./convert');
 var DeviceRotationApp = require('./deviceorientation-behavior-app');
-var ScrollApp = require('./scroll-behavior-app');
 var timeUtil = require('./timeUtil');
+import {ScrollBehaviorApp as ScrollApp} from './scroll-behavior-app'
 
 var deviceRotationApp = new DeviceRotationApp(domUtil, convert, timeUtil);
 var scrollApp = new ScrollApp(domUtil, convert);
